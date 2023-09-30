bdfd9cdba553fe915f8f2e4b27123bfb94b66810
Matt Barry <matt@hazelmollusk.org>
2023-09-30 07:41:37 -0400

    bumpversion

--- docs/conf.py
+++ docs/conf.py
@@ -44,11 +44,11 @@ if os.getenv('SPELLCHECK'):
 
 source_suffix = '.rst'
 master_doc = 'index'
-project = 'Python-Project-Skeleton'
-year = '2020'
-author = 'Joao MC Teixeira'
+project = 'Vaticinator'
+year = '2023'
+author = 'Matt Barry'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.11.3'
+version = release = '0.0.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/vaticinator/__init__.py
+++ src/vaticinator/__init__.py
@@ -1,4 +1,4 @@
 """Vaticinator"""
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 from .vaticinator import Vaticinator  

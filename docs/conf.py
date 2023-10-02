56e736fd4bda770294573164e2887d6b4dcd86f7
Matt Barry <matt@hazelmollusk.org>
2023-10-02 12:00:43 -0400

    version 0.0.4 - $COMMIT_MSG

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ project = 'Vaticinator'
 year = '2023'
 author = 'Matt Barry'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.7'
+version = release = '0.0.8'
 
 pygments_style = 'trac'
 templates_path = ['.']
